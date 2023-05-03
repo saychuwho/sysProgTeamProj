c4e5fd033789375addf474835e3ec60d86608348
saychuwho <saychuwho@dgist.ac.kr>
2023-05-03 20:57:00 +0900

    end the job

--- TempKeypadCon.c
+++ TempKeypadCon.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <term.h>  
+#include <termios.h>  
+#include <unistd.h>  
+
+int getch(void)  
+{  
+  int ch;  
+  struct termios buf;  
+  struct termios save;  
+  
+   tcgetattr(0, &save);  
+   buf = save;  
+   buf.c_lflag &= ~(ICANON|ECHO);  
+   buf.c_cc[VMIN] = 1;  
+   buf.c_cc[VTIME] = 0;  
+   tcsetattr(0, TCSAFLUSH, &buf);  
+   ch = getchar();  
+   tcsetattr(0, TCSAFLUSH, &save);  
+   return ch;  
+} 
 
 /*how to make this
 - 어떤 key가 입력되면, 그 key가 눌릴 때 마다 증가하는 수가 늘어난다. abc / def / ... 이런식으로 누를 때 마다 입력되는 알파벳이 달라지기 때문에 이를 구현한다.
@@ -7,7 +27,14 @@
 - 이 과정을 while로 돌리면 되지 않을까.
 */
 
+
 int main(){
+    int pushed_num[10];
+    for(int i=0;i<10;i++)
+        pushed_num[i] = 0;
+    int last_pushed = 0;
+
+    
 
     return 0;
 }
